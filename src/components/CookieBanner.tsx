3912ea4cf6e5aea3022dcd1e76e8f71ed8509888
Eugen Ostafi <eugen.ostafi@isa.utm.md>
2025-08-04 20:34:37 +0300

    :bug: build bug fix

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useLanguage } from './LanguageContext';
 import { useNavigate } from 'react-router-dom';
 import './CookieBanner.css';
